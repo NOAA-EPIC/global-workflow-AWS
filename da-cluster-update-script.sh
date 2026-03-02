e81f0e544ceedd2e7ea11a82d60fd5dd714ccaf3
Kris Booker <kristopher.booker@tomorrow.io>
2026-03-02 08:08:21 -0700

    Amending base image

---
+++ da-cluster-update-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /opt
+cd spack-stack
+. ./setup.sh
+cd /opt/spack-stack/envs/ue-oneapi-2024.2.1
+spack env activate -p .
+spack install --add grib-util%oneapi@2024.2.1@1.4.0
+spack install --add prod-util%oneapi@2024.2.1@2.1.1
+spack install --add cdo%oneapi@2024.2.1@2.3.0
+spack module lmod refresh -y
+spack stack setup-meta-modules
+spack env deactivate
